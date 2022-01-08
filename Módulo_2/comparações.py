8bdab387c77b63d37278b379ac50e121d39a4a91
slocksert <88241304+slocksert@users.noreply.github.com>
2022-01-08 15:53:39 -0300

    Add files via upload

---
+++ Módulo_2/comparações.py
@@ -0,0 +1,16 @@
+maior_de_idade = True
+possui_carteira_de_trabalho = False
+esta_trabalhando_atualmente = True
+possui_veiculo_proprio = False
+menor_de_idade = False
+passagem_comprada = False
+passaporte = False
+
+""" if maior_de_idade == True and possui_carteira_de_trabalho == True:
+    print("Você pode trabalhar aqui")
+else:
+    print("Você não pode trabalhar aqui") """
+
+#print(possui_veiculo_proprio == False and possui_carteira_de_trabalho == True)
+
+print((passaporte==True and passagem_comprada==True) and (menor_de_idade==False))

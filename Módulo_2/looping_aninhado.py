8bd18ea94a1bc350570e1caf3cffeb467aaea06e
slocksert <88241304+slocksert@users.noreply.github.com>
2022-01-08 16:21:56 -0300

    Add files via upload

---
+++ Módulo_2/looping_aninhado.py
@@ -0,0 +1,6 @@
+celulares=['Asus','Samsung','Sony','Iphone']
+versoes=['Plus','Premium Plus','Premium Deluxe','Plus Premium Ultra']
+
+for celular in celulares:
+    for versao in versoes:
+        print(f"{celular} {versao}")
---
+++ Módulo_2/looping_for.py
@@ -0,0 +1,13 @@
+#Desafio 1
+
+""" x = range(18,111)
+
+for valor in x:
+    print(valor) """
+
+#Desafio 2
+
+""" passos = range(1,11)
+
+for passo in passos:
+    print(f"Realizando passo {passo}") """

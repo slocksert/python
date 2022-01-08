525fcfd7b00cf0aa3ca229eaf8876e1b9017d604
slocksert <88241304+slocksert@users.noreply.github.com>
2022-01-08 15:39:12 -0300

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,12 @@
+hair_size = 64
+
+if hair_size <= 20:
+    print("R$50 Reais")
+
+elif 21 <= hair_size <= 30:
+    print("R$70 Reais")
+
+elif 31 <= hair_size <= 50:
+    print("R$100 Reais")
+else:
+    print("Favor consultar a recepção")

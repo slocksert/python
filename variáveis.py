94c4cd4bfb3d7df0447608f9a34f63705276fcfa
slocksert <88241304+slocksert@users.noreply.github.com>
2022-01-08 15:51:05 -0300

    Delete variáveis.py

--- variáveis.py
+++
@@ -1,7 +0,0 @@
-#DESAFIO 1
-#COPIE E COLE AS SEGUINTES LINHAS DE CÓDIGO PARA SEU EDITOR DE CÓDIGO E DESCUBRA QUAL É O TIPO DE CADA UM DESSES DADOS
-
-variavel_1 = 25.87 #número decimal
-variavel_2 = True #valor booleano
-variavel_3 = "Bom dia!" #string
-variavel_4 = 15 #número

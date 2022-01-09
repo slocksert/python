17796d959fd614f4252bb2f65da41d32b880886e
Rildo Silva <88241304+slocksert@users.noreply.github.com>
2022-01-09 19:02:35 -0300

    Add files via upload

---
+++ Módulo_4/listas.py
@@ -0,0 +1,14 @@
+#Desafio 1
+lista = ['Computador','Celular','Garrafinha']
+print(lista[lista.index('Computador')])
+
+#Desafio 2
+numeros = list(range(10,132))
+print(numeros)
+
+#Desafio 3
+print(lista + numeros)
+
+#Desafio 4
+lista_matriz = [['Computador',5000],['Celular ', 1200,['Garrafinha', 10]]]
+print(lista_matriz[0][0])

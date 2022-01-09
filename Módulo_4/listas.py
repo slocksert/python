553fbacc6c6ec492b4c10bd38541a4f0a9c17635
Rildo Silva <88241304+slocksert@users.noreply.github.com>
2022-01-09 19:34:13 -0300

    Update listas.py

--- Módulo_4/listas.py
+++ Módulo_4/listas.py
@@ -1,4 +1,4 @@
-#Desafio 1
+""" #Desafio 1
 lista = ['Computador','Celular','Garrafinha']
 print(lista[lista.index('Computador')])
 
@@ -11,4 +11,42 @@ print(lista + numeros)
 
 #Desafio 4
 lista_matriz = [['Computador',5000],['Celular ', 1200,['Garrafinha', 10]]]
-print(lista_matriz[0][0])
+print(lista_matriz[0][0]) """
+
+""" # adicionar item ao final da lista
+numeros = [1,7,2,4,6,12,3,5,65,23,15]
+lista2 = ['metodo','estudos']
+lista = ['teste','testando','123'] """
+""" lista.append('456')
+print(lista)
+#unir listas
+lista.extend(lista2)
+print(lista) """
+""" #adicionar lista """
+""" nova_lista = lista + lista2
+print(nova_lista) """
+""" #inserir """
+""" print(lista2[1])
+lista2.insert(2, 'Outdoor')
+print(lista2) """
+""" #extrair com base no indice
+lista2_123 = lista2.pop(0)
+print(lista2_123) """
+""" #remover item da lista
+lista.remove(teste)
+print(lista) """
+""" #contando ocorrencia de um valor
+print(lista.count(0)) """
+""" #resetar
+lista.clear()
+print(lista) """
+
+""" #ordem crescente
+numeros.sort()
+print(numeros) """
+""" #ordem decrescente
+numeros.sort(reverse=True)
+print(numeros) """
+""" #lista ao contrário
+numeros.reverse()
+print(numeros) """

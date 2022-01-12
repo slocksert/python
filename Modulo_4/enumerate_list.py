10d4b2a31eb46a542bf45787e78c96b52801711d
Rildo Silva <88241304+slocksert@users.noreply.github.com>
2022-01-12 00:37:59 -0300

    Delete Modulo_4 directory

--- Modulo_4/enumerate_list.py
+++
@@ -1,8 +0,0 @@
-# Enumerate
-# Desafio 1
-frutas = ['Maça','Laranja','Morango','Limao']
-for indice, fruta in enumerate(frutas,0):
-    if indice == 3:
-        print(f'{indice} {fruta} EM PROMOÇÃO')
-    else:
-        print(indice, fruta)
--- Modulo_4/range.py
+++
@@ -1,6 +0,0 @@
-#Range
-for numero in range(10,201,10): # 3º parametro é o step
-    print(numero)
-#criar listas mais rapidamente
-resultado = list(range(10,101,10))
-print(f'O resultado é {resultado}')

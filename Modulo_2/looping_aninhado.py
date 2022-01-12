748e5694ac91eb7f04f53dc870be5f4b3f15fd5b
Rildo Silva <88241304+slocksert@users.noreply.github.com>
2022-01-12 00:37:43 -0300

    Delete Modulo_2 directory

--- Modulo_2/looping_aninhado.py
+++
@@ -1,6 +0,0 @@
-celulares=['Asus','Samsung','Sony','Iphone']
-versoes=['Plus','Premium Plus','Premium Deluxe','Plus Premium Ultra']
-
-for celular in celulares:
-    for versao in versoes:
-        print(f"{celular} {versao}")
--- Modulo_2/operador_ternário.py
+++
@@ -1,3 +0,0 @@
-velocidade = 80
-
-print("Você foi multado") if velocidade > 100 else print("Siga em frente")

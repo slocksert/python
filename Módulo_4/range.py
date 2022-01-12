e37c2e6b8268f4fbe057f453e7074c8e9495b85c
slocksert <rsdlj01@gmail.com>
2022-01-11 23:40:04 -0300

    lists

---
+++ Módulo_4/range.py
@@ -0,0 +1,6 @@
+#Range
+for numero in range(10,201,10): # 3º parametro é o step
+    print(numero)
+#criar listas mais rapidamente
+resultado = list(range(10,101,10))
+print(f'O resultado é {resultado}')

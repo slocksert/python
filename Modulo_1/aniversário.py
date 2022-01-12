9bbe497f16a39b822a04b6eadb5ab10ca7349bb6
Rildo Silva <88241304+slocksert@users.noreply.github.com>
2022-01-12 00:37:33 -0300

    Delete Modulo_1 directory

--- Modulo_1/aniversário.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-aniversário = datetime.strptime(input("Quando é o seu aniversário? (Utilize o formato ano/mês/dia) \n"), "%Y/%m/%d")
-data = datetime.now()
-prazo = aniversário - data
-print(f"Só faltam {prazo.days} dias para o seu aniversário!")
-print(data)

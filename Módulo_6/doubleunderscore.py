9cfb2a889f587db072244782679506ab66421bbb
slocksert <rsdlj01@gmail.com>
2022-01-15 00:50:16 -0300

    att

--- Módulo_6/doubleunderscore.py
+++ Módulo_6/doubleunderscore.py
@@ -8,16 +8,10 @@ class Pessoa:
     def resultado(self):
         print(f'Seu nome de super-herói é {self.nome} e suas habilidades são:\n{self.habilidade1}\n{self.habilidade2}\n{self.habilidade3}')
 
-""" nome = input('Digite seu nome de super-herói:\n')
+nome = input('Digite seu nome de super-herói:\n')
 habilidade1 = input('Digite sua primeira habilidade:\n')
 habilidade2 = input('Digite sua segunda habilidade:\n')
 habilidade3 = input('Digite sua terceira habilidade:\n')
 
 pessoa=Pessoa(nome, habilidade1, habilidade2, habilidade3)
-pessoa.resultado() """
-
-class Pessoas:
-    def __repr__(self):
-        return repr()
-
-print(Pessoas.mro())
+pessoa.resultado()
